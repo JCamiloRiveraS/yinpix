f9bfb470303e4216119a6662e18ee9a828bf2e35
Camilo Rivera <102061094+JCamiloRiveraS@users.noreply.github.com>
2025-12-28 18:23:12 -0500

    upload project
    pito

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <style>
+        body {
+            background: #73f8de88;
+            font-size: 18px;
+        }
+        h1{
+            color: #1f1401;
+            text-align: center
+        }
+    </style>
+    <h1>
+        me gustan los furros con olor a patas y siy bien mamona
+    </h1>
+    <img src="https://i.pinimg.com/736x/c2/2f/3f/c22f3f66e4ab14d52999b8d3db2cdbb6.jpg" width="200" alt="">
+</body>
+
+</html>
